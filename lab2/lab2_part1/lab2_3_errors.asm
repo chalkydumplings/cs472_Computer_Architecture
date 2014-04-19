4d8d321456c4ebd04a209d28c1ac9aecea8d908a
drakebridgewater <drake.bridgewater@gmail.com>
2014-04-18 20:09:10 -0700

    fixing file structure

---
+++ lab2/lab2_part1/lab2_3_errors.asm
@@ -0,0 +1,23 @@
+		AREA lab2_3_errors, CODE, READONLY	
+		ENTRY
+	
+	ADR r0, A
+	ADR r2, C
+ADR r4, D
+	ADR r6, E
+	ADR r8
+	ADR r10, Z
+A   DCD  4
+C   DCD  
+D   DCD  3
+E   DCD  -12
+F   DCD  5
+Z  	DCD  0
+
+
+	ADD r2,r2
+	ADD r2,r2,r0
+
+	MULS r10,r6,r8	
+	SUB r10,r2,r10
+
