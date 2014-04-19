c8f1b9f3adce76c892905af93b276a2553997c46
Ryan Phillips <wordsandcolor@gmail.com>
2014-04-18 20:43:25 -0700

    adding another palindrome.asm

---
+++ lab2.new/lab2_part2/palindrome.asm
@@ -0,0 +1,30 @@
+; source: http://www.ing.unlp.edu.ar/electrotecnia/arcom1/09-Clements-Chap09-ARM.pdf
+
+start
+		ldr	r0,-string
+		mov	r1,r0
+loop		ldrb	r2,[r1],#1
+		cmp	r2,#0
+		bne	loop
+		sub	r1,r1,#2
+		bl	pal
+
+pal		mov	r10, #0x0
+loop2		ldrb	r3, [r0]
+		ldrb	r4, [r1]
+		cmp	r3, r4
+		bne	notpal
+
+		cmp 	r0, r1
+		beq	waspal
+		add	r2,r0,#1
+		cmp	r2,r1
+		beq	waspal
+
+		add	r0,r0,#1
+		sub	r1,r1,#1
+		b 	loop2
+
+waspal		mov	r10, #0x1
+notpal		mov	pc,lr
+	
