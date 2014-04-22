75331f1320db67e83f269fa1ebbe4f4a61e90782
Ryan Phillips <wordsandcolor@gmail.com>
2014-04-21 20:29:30 -0700

    fixing merge conflict

--- Assignment3/NullTerminatingString/NullTerminatingString.asm
+++ Assignment3/NullTerminatingString/NullTerminatingString.asm
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 		AREA NullTerminatingString, CODE, READWRITE	
 		ENTRY
 		;;modification of code from section 3.9.11
@@ -15,19 +14,5 @@ loop	LDRB	R2, [R0], #1 ; grab first character, increment pointer
 
 src = 		"aaaaaaaazzzzzz", &0A, &0D, 0
 		
-=======
-		AREA NullTerminatingString, CODE, READONLY	
-		ENTRY
-		;;copy of code from section 3.9.11
-		
-Banner	ADR R1, String
-Bnner1	LDRB	R0, [R10], #1
-		CMP 	R0, #0
-		SVCNE 	WriteC
-		BNE 	Bnner1
-		MOV		PC, R14
 
-String =	"A String goes here!", &0A, &0D, 0
-WriteC	EQU		0
->>>>>>> 429ce864761d563cd133c94e92b8270e282d7844
 		END
