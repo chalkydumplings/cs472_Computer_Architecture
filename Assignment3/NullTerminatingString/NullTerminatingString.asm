19f0cd5b2cd36e9f70323b9b6eba694bd9299103
Ryan Phillips <wordsandcolor@gmail.com>
2014-04-21 20:17:18 -0700

    3.30 kind of works now (if you give it mem)
    via memory map

--- Assignment3/NullTerminatingString/NullTerminatingString.asm
+++ Assignment3/NullTerminatingString/NullTerminatingString.asm
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+		AREA NullTerminatingString, CODE, READWRITE	
+		ENTRY
+		;;modification of code from section 3.9.11
+		;;copies from r0 to r1
+		
+copystr	ADR R0, src ; r0 points at string 
+		LDR R1, =0x0000005f   ; a location of safe mem?
+loop	LDRB	R2, [R0], #1 ; grab first character, increment pointer
+		CMP 	R2, #0x00 ; are we at the end?
+		STRB 	R2, [r1], #1 ; if not, copy to address location (from r1)
+		;ADD		R1, #1 ; increment r1
+		BNE 	loop ; if not at the end... we move onto the next character
+		MOV		PC, R14
+
+src = 		"aaaaaaaazzzzzz", &0A, &0D, 0
+		
+=======
 		AREA NullTerminatingString, CODE, READONLY	
 		ENTRY
 		;;copy of code from section 3.9.11
@@ -11,4 +29,5 @@ Bnner1	LDRB	R0, [R10], #1
 
 String =	"A String goes here!", &0A, &0D, 0
 WriteC	EQU		0
+>>>>>>> 429ce864761d563cd133c94e92b8270e282d7844
 		END
