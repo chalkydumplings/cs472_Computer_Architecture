39d912a981212af2d4531d46cad9ba888c4611d9
drakebridgewater <drake.bridgewater@gmail.com>
2014-04-21 22:41:19 -0700

    A3 is done and submitted

--- Assignment3/NullTerminatingString/NullTerminatingString.asm
+++ Assignment3/NullTerminatingString/NullTerminatingString.asm
@@ -1,23 +1,16 @@
-		AREA NullTerminatingString, CODE, READWRITE	
-		ENTRY
-		;;modification of code from section 3.9.11
-		;;copies from r0 to r1
-		
-copystr	ADR R0, src ; r0 points at string 
-		LDR R1, =0x0000005f   ; a location of safe mem?
-loop	LDRB	R2, [R0], #1 ; grab first character, increment pointer
-		CMP 	R2, #0x00 ; are we at the end?
-		STRB 	R2, [r1], #1 ; if not, copy to address location (from r1)
-		;ADD		R1, #1 ; increment r1
-		BNE 	loop ; if not at the end... we move onto the next character
-		MOV		PC, R14
+					AREA NullTerminatingString, CODE, READWRITE	
+					ENTRY
+					;;modification of code from section 3.9.11
+					;;copies from r0 to r1
+					
+copystr		ADR R0, src ; r0 points at string 
+					LDR R1, =0x0000005f   ; a location of safe mem?
 
-<<<<<<< HEAD
-String =	"A String goes here!"
-WriteC	EQU		0
-=======
+loop			LDRB	R2, [R0], #1 ; grab first character, increment pointer
+					CMP 	R2, #0x00 ; are we at the end?
+					STRB 	R2, [r1], #1 ; if not, copy to address location (from r1)
+					BNE 	loop ; if not at the end... we move onto the next character
+					MOV		PC, R14
+
 src = 		"aaaaaaaazzzzzz", &0A, &0D, 0
-		
-
->>>>>>> 75331f1320db67e83f269fa1ebbe4f4a61e90782
-		END
+					END
