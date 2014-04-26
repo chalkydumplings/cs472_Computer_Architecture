490f4907f0b519fc2397be9175f465823a333a4e
drakebridgewater <drake.bridgewater@gmail.com>
2014-04-25 22:22:10 -0700

    endianness not working.....

--- Lab3/endianness_testing/endianness_testing.asm
+++ Lab3/endianness_testing/endianness_testing.asm
@@ -1,11 +1,12 @@
 	AREA endianness_testing, CODE, READWRITE	
 		ENTRY
+;A function which tests the endianness of the system, and flips it as requested. This will be expanded upon in a later lab to examine the concept of endian neutral programming.
 
-
-	MOV	r1, #64;
-
+	ADR 	R1, src 
+	LDRB	R2, [R1], #1
+	BEQ     R2, 
 
 l 	b 		l	; infinite loop
-	
+src = 			"ABC"	
 	END
 	
