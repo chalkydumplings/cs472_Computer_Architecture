1c752d211a9edd54165229e0c5c87ecc040c0a3b
drakebridgewater <drake.bridgewater@gmail.com>
2014-04-27 13:07:11 -0700

    a4 in progress

--- test_project/test.asm
+++ test_project/test.asm
@@ -1,16 +1,30 @@
 			AREA test, CODE, READWRITE	
 			ENTRY
 			
-			LDR 	r0, =0x00000000
-			LDR		R1, =0x11111111
-			LDR		R2, =0x22222222
-			LDR		R3, =0x33333333
-			LDR		R4, =0x44444444
-			LDR		R5, =0x55555555
-			LDR		R6, =0x66666666
+			LDR		R0, #0		;operand 2 flag
+			LDR		R1, #0x21
+			LDR		R2, #0x401
+			LDR 	R3, #0x1000
 			
-			ADR 	R13, Stack
-			STMIB r7!,{r0-r2,r4}
-			SPACE 20
-Stack		SPACE 20
+			
+			ADR		R5, R1	;points to R1
+			ADR		R6, R2	;points to R2
+			ADR		R7, R3	;points to R3
+			
+			
+loop		CMP		;fist bit of r2 		
+					;if == 0 branch to shift
+					;add to result
+shift		LSL			;left shift
+					;if not reached end
+			BEQ loop		;==1 branch to loop
+			
+			
+			CMP 	R0, #1		
+			BEQ		endless		;if we have
+			ADR		R6, R3		;change pointer to R7
+			LDR		R0, #1		;set flag saying switched operand 2
+			B 		loop 		;now step through mult everything by r3
+			
+endless 	B endless			
 			END
