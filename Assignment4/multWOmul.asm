a70a315180d74f562ef1f5b38601446208480cb4
drakebridgewater <drake.bridgewater@gmail.com>
2014-04-28 21:53:39 -0700

    all but 3.8 done

---
+++ Assignment4/multWOmul.asm
@@ -0,0 +1,31 @@
+
+			AREA test, CODE, READWRITE	
+			ENTRY
+			
+			LDR		R0, #0		;operand 2 flag
+			LDR		R1, #0x21
+			LDR		R2, #0x401
+			LDR 	R3, #0x1000
+			
+			
+			ADR		R5, R1	;points to R1
+			ADR		R6, R2	;points to R2
+			ADR		R7, R3	;points to R3
+			
+			
+loop		CMP		;fist bit of r2 		
+			BEQ		shift		;if == 0 branch to shift
+					;add to result
+shift		LSL			;left shift
+					;if not reached end
+			BEQ loop		;==1 branch to loop
+			
+			
+			CMP 	R0, #1		
+			BEQ		endless		;if we have
+			ADR		R6, R3		;change pointer to R7
+			LDR		R0, #1		;set flag saying switched operand 2
+			B 		loop 		;now step through mult everything by r3
+			
+endless 	B endless			
+			END
--- test_project/test.asm
+++ test_project/test.asm
@@ -1,30 +1,17 @@
-			AREA test, CODE, READWRITE	
-			ENTRY
-			
-			LDR		R0, #0		;operand 2 flag
-			LDR		R1, #0x21
-			LDR		R2, #0x401
-			LDR 	R3, #0x1000
-			
-			
-			ADR		R5, R1	;points to R1
-			ADR		R6, R2	;points to R2
-			ADR		R7, R3	;points to R3
-			
-			
-loop		CMP		;fist bit of r2 		
-					;if == 0 branch to shift
-					;add to result
-shift		LSL			;left shift
-					;if not reached end
-			BEQ loop		;==1 branch to loop
-			
-			
-			CMP 	R0, #1		
-			BEQ		endless		;if we have
-			ADR		R6, R3		;change pointer to R7
-			LDR		R0, #1		;set flag saying switched operand 2
-			B 		loop 		;now step through mult everything by r3
-			
-endless 	B endless			
-			END
+    AREA test, CODE, READWRITE    
+		ENTRY
+		
+		; multiply input by 33 -> original += 4 byte shift
+		MOV        R0, #4095             ; 
+		MOV        R1, R0, LSL #5         ; r1 = r0 * 2^5 
+		ADD        R2, R1, R0            ; r2 = r1 + r0
+									; hooray, r2 contains r0 * 33
+									
+		; multiply input by 33 -> original += 4 byte shift
+		MOV        R0, R2                 ; 
+		MOV        R1, R0, LSL #10        ; r1 = r0 * 2^5 
+		ADD        R2, R1, R0            ; r2 = r1 + r0
+									; hooray, r2 contains r0 * 33
+		space     10
+	
+		END
