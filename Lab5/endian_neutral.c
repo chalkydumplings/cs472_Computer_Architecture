46dc912696cd6e1b33f3c98c7f46c6c0225097da
Drake Bridgewater <bridgewd@flip2.engr.oregonstate.edu>
2014-05-30 15:32:14 -0700

    endian neutral test on beagle bone

--- Lab5/endian_neutral.c
+++ Lab5/endian_neutral.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
+//#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
 
 int main(int argc, char **argv)
 {
-        printf("%d\n",IS_BIG_ENDIAN); //0 if false
+        printf("%d\n",IS_BIG_ENDIAN()); //0 if false
 
         short val;
         char *p_val;
         p_val = (char *) &val;
 
-        if (IS_BIG_ENDIAN){
-          p_val[0] = 0x12;
-          p_val[1] = 0x34;
+        if (IS_BIG_ENDIAN()){
+			printf("\t\tBIG ENDIAN\n\n");
+			p_val[0] = 0x12;
+			p_val[1] = 0x34;
         } else {
-          p_val[0] = 0x34;
-          p_val[1] = 0x12;
+			printf("\t\tlittle ENDIAN\n\n");
+			p_val[0] = 0x34;
+			p_val[1] = 0x12;
         }
         printf("%x\n", val);
 
         return 0;
+}
+int IS_BIG_ENDIAN(){
+	return (*(uint16_t *)"\0\xff" < 0x100);
 }
